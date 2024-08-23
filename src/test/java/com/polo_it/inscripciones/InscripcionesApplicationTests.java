75754da7663a743e82b565dd58e5fcf46b4b45a5
MMCMerino <mariamartamerino@gmail.com>
2024-08-23 17:40:44 -0300

    primer commit

---
+++ src/test/java/com/polo_it/inscripciones/InscripcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polo_it.inscripciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscripcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
